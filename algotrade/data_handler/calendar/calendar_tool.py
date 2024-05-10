4df6aad597bfba7e8ae1985ba4ec1c2d023de89d
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-05-10 20:10:12 +0530

    MarketTimings in MarketCalendar.

--- algotrade/data_handler/calendar/calendar_tool.py
+++ algotrade/data_handler/calendar/calendar_tool.py
@@ -3,6 +3,7 @@ from typing import List, NewType
 from algotrade.data_handler.calendar.calendar_data import (
     MarketHolidayType,
     WorkingDayDate,
+    MarketTimingType
 )
 
 
@@ -12,7 +13,8 @@ class MarketCalendar:
         today: NewType("DateFormat", str),
         market_name: str,
         market_holidays: List[MarketHolidayType],
+        market_timings: MarketTimingType
     ):
         self.today = today
-        self.working_day = WorkingDayDate(self.today, market_holidays)
+        self.working_day = WorkingDayDate(self.today, market_holidays, market_timings=market_timings)
         self.market_name = market_name
