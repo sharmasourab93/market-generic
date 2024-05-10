2d1da65857e790d3696a66b703e6bc3a8ee3facb
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-05-10 22:11:10 +0530

    Configurations/Utils setup.

--- trade/utils/__init__.py
+++ trade/utils/__init__.py
@@ -1,2 +1,3 @@
 from trade.utils.gsheet_util import GSheetCredType, GSpreadManager as GoogleSheet
+from trade.utils.logger import LoggingType, LogConfig as Logger
 from trade.utils.telegram_api import TelegramBot as Telegram
