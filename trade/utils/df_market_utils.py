300dc108a7ef3824e3ed405acb1309553d08f9bb
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-05-12 22:44:01 +0530

    Updated Utils.

---
+++ trade/utils/df_market_utils.py
@@ -0,0 +1,21 @@
+from pandas import DataFrame
+from abc import ABC
+
+
+class MarketDFUtils(ABC):
+
+    def data_remove_quotes_with_na(self, data: DataFrame, column: str = "close") -> \
+            DataFrame:
+        return data.loc[~data[column].isna(), :]
+
+    def calculate_pct_change(
+        self,
+        data: DataFrame,
+        column1: str = "close",
+        column2: str = "prev_close",
+        change_column: str = "pct_change",
+    ) -> DataFrame:
+
+        data[change_column] = ((data[column1] - data[column2]) / data[column2]) * 100
+
+        return data
