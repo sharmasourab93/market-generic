a9af883a237238a9dbc29e11c06a5a55249b8295
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-05-26 21:36:55 +0530

    Eliminating Circular Import # 4

--- trade/nse/__init__.py
+++ trade/nse/__init__.py
@@ -1,6 +1,1 @@
-def initialize():
-    from trade.nse.nse_stock import NSEStock, NSE_TOP
-    from trade.nse.nse_all_stocks import AllNSEStocks
-
-
-initialize()
+from trade.nse.nse_all_stocks import AllNSEStock
