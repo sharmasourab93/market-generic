e4487f413787dceb5446eff15202436eb51d384c
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-06-12 20:51:39 +0530

    Modified Test to rectify mistakes.

--- tests/test_technicals/test_option_chain/test_index_option_chain.py
+++ tests/test_technicals/test_option_chain/test_index_option_chain.py
@@ -34,16 +34,16 @@ def index_option_chain(dated, config, as_month):
     return option_chain
 
 
-def test_option_chain_object(stock_option_chain, dated):
+def test_option_chain_object(index_option_chain, dated):
 
-    for oc_obj in stock_option_chain:
+    for oc_obj in index_option_chain:
         assert isinstance(oc_obj, IndexOptionChainAnalysis)
         assert oc_obj.symbol in INDICES_MAPPING.keys()
         assert oc_obj.dated == dated
 
 
-def test_pcr_verdict(stock_option_chain):
-    for oc_obj in stock_option_chain:
+def test_pcr_verdict(index_option_chain):
+    for oc_obj in index_option_chain:
         assert oc_obj.pcr_verdict(0.3) == "Over Sold"
         assert oc_obj.pcr_verdict(0.5) == "Very Bearish"
         assert oc_obj.pcr_verdict(0.7) == "Bearish"
@@ -54,13 +54,13 @@ def test_pcr_verdict(stock_option_chain):
         assert oc_obj.pcr_verdict(-0.1) == "Invalid"
 
 
-def test_overall_pcr(stock_option_chain):
-    for oc_obj in stock_option_chain:
+def test_overall_pcr(index_option_chain):
+    for oc_obj in index_option_chain:
         assert isinstance(oc_obj.overall_pcr, float)
 
 
-def test_strikes(stock_option_chain):
-    for oc_obj in stock_option_chain:
+def test_strikes(index_option_chain):
+    for oc_obj in index_option_chain:
         assert isinstance(oc_obj.strike_price, int) or isinstance(oc_obj.strike_price, float)
         assert isinstance(oc_obj.strike_min, int) or isinstance(oc_obj.strike_min,
                                                               float)
@@ -69,8 +69,8 @@ def test_strikes(stock_option_chain):
         assert isinstance(oc_obj.strike_range, range)
 
 
-def test_option_chain_output(stock_option_chain):
-    for oc_obj in stock_option_chain:
+def test_option_chain_output(index_option_chain):
+    for oc_obj in index_option_chain:
         result = oc_obj.option_chain_output()
         assert isinstance(result, dict)
         assert "near_strike_info" in result.keys()
