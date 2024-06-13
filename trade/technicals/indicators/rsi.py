cb6071e4cb4487a033695dc72cc42e8d42c11fa3
Sourab Sharma <sharmasourab93@users.noreply.github.com>
2024-06-13 13:06:42 +0530

    Included RSI

---
+++ trade/technicals/indicators/rsi.py
@@ -0,0 +1,34 @@
+from typing import Union, Dict
+import pandas as pd
+from pandas import DataFrame
+from abc import ABC, abstractmethod
+from trade.technicals.indicators.generic_indicator import GenericIndicator
+
+
+class RSI(GenericIndicator):
+    __name__ = "Relative Strength Index (RSI)"
+
+    def __init__(self, data: DataFrame, period: int = 14):
+        super().__init__(data)
+        self.data = data
+        self.period = period
+
+    def calculate_rsi(self) -> DataFrame:
+        delta = self.data["close"].diff().dropna()
+        up, down = delta.copy(), delta.copy()
+        up[up < 0] = 0
+        down[down > 0] = 0
+        roll_up = up.rolling(window=self.period).mean()
+        roll_down = down.rolling(window=self.period).mean().abs()
+        RS = roll_up / roll_down
+        RSI = 100.0 - (100.0 / (1.0 + RS))
+        self.data["RSI"] = RSI
+        return self.data[::-1]
+
+    @classmethod
+    def apply_indicator(
+        cls,
+        data: pd.DataFrame,
+        period: int = 14,
+    ) -> DataFrame:
+        return cls(data, period).calculate_rsi()
